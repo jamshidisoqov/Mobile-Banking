bf76591171191e7a2bfcec7e58c79c2a225ce08d
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-12-27 14:26:02 +0500

    shimmer_added

--- app/src/main/java/uz/gita/mobile_banking/presentation/ui/home/pages/accounts/AccountsScreen.kt
+++ app/src/main/java/uz/gita/mobile_banking/presentation/ui/home/pages/accounts/AccountsScreen.kt
@@ -1,8 +1,13 @@
 package uz.gita.mobile_banking.presentation.ui.home.pages.accounts
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import uz.gita.mobile_banking.R
 
 // Created by Jamshid Isoqov on 12/24/2022
+@AndroidEntryPoint
 class AccountsScreen : Fragment(R.layout.screen_accounts) {
+
+
+
 }
