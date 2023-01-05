70d7b9d50feeeb28419bb70f51a4ead03fedc226
jamshidisoqov <isoqovjamshid01@gmail.com>
2023-01-05 14:31:35 +0500

    payment screen added

---
+++ app/src/main/java/uz/gita/mobile_banking/presentation/ui/payment/PaymentsScreen.kt
@@ -0,0 +1,8 @@
+package uz.gita.mobile_banking.presentation.ui.payment
+
+import androidx.fragment.app.Fragment
+import uz.gita.mobile_banking.R
+
+// Created by Jamshid Isoqov on 1/5/2023
+class PaymentsScreen : Fragment(R.layout.screen_payments){
+}
---
+++ app/src/main/java/uz/gita/mobile_banking/presentation/ui/transfer/dialog/TransferDetailDialog.kt
@@ -0,0 +1,8 @@
+package uz.gita.mobile_banking.presentation.ui.transfer.dialog
+
+import androidx.fragment.app.DialogFragment
+import uz.gita.mobile_banking.R
+
+// Created by Jamshid Isoqov on 1/5/2023
+class TransferDetailDialog : DialogFragment(R.layout.dialog_transfer_detail){
+}
